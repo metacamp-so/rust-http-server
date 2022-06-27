ae94e53963645695b8e22199ebea82d4325dfa89
jakeyvee <jakeyvee@gmail.com>
2022-06-28 00:02:41 +0800

    implemented TCP listener and log requests

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+use std::io::Read;
 pub struct Server {
   addr: String,
 }
@@ -9,6 +11,23 @@ impl Server {
   }
 
   pub fn run(self) { // follows ownership rule, use & if don't want to take ownership
-      println!("Listening on {}", self.addr)
+      println!("Listening on {}", self.addr);
+
+      let listener = TcpListener::bind(&self.addr).unwrap(); // we unwrap to convert into unrecoverable error
+
+      loop { // loops infinitely
+        match listener.accept() {
+          Ok((mut stream, _)) => {
+            let mut buffer = [0; 1024]; // mutable buffer for .read to write 1024 is ok for our simple server
+            match stream.read(&mut buffer) {
+              Ok(_) => {
+                println!("Received a request: {}", String::from_utf8_lossy(&buffer))
+              },
+              Err(e) => println!("Failed to read from connection: {}", e),
+            }
+          },
+          Err(e) => println!("Failed to establish a connection: {}", e),
+        }
+      }
   }
 }
