c8b50594b3d18bf8eb9a8fd2c2bb2aa5b1403981
jakeyvee <jakeyvee@gmail.com>
2022-06-28 00:33:33 +0800

    custom errors

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -3,3 +3,4 @@ pub mod method;
 
 pub use method::Method;
 pub use request::Request;
+pub use request::ParseError;
--- src/http/request.rs
+++ src/http/request.rs
@@ -1,5 +1,8 @@
 use super::method::Method;
 use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+
 pub struct Request {
   path: String,
   query_string: Option<String>, // should be optional not mandatory
@@ -7,10 +10,44 @@ pub struct Request {
 }
 
 impl TryFrom<&[u8]> for Request { // we are taking in a ref byte array
-  type Error = String;
+  type Error = ParseError;
 
   // GET /search?name=abc&sort=1 HTTP/1.1
   fn try_from(value: &[u8]) -> Result <Self, Self::Error> {
     unimplemented!()
   }
+}
+
+pub enum ParseError {
+  InvalidRequest,
+  InvalidEncoding,
+  InvalidProtocol,
+  InvalidMethod,
+}
+
+impl Debug for ParseError {
+  fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+    write!(f, "{}", self.message())
+  }
+}
+
+impl Display for ParseError {
+  fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+    write!(f, "{}", self.message())
+  }
+}
+
+impl ParseError {
+  fn message(&self) -> &str {
+    match self {
+      ParseError::InvalidRequest => "Invalid Request",
+      ParseError::InvalidEncoding => "Invalid Encoding",
+      ParseError::InvalidProtocol => "Invalid Protocol",
+      ParseError::InvalidMethod => "Invalid Method",
+    }
+  }
+}
+
+impl Error for ParseError {
+
 }
