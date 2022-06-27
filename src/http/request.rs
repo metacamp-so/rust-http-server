803ede0d8eb2fb5b56dd1ddbc6f86abd93980348
jakeyvee <jakeyvee@gmail.com>
2022-06-28 00:16:25 +0800

    type conversion using trait, TryFrom

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,6 +1,16 @@
 use super::method::Method;
+use std::convert::TryFrom;
 pub struct Request {
   path: String,
   query_string: Option<String>, // should be optional not mandatory
   method: Method, // represent as enum we introduced common methods
+}
+
+impl TryFrom<&[u8]> for Request { // we are taking in a ref byte array
+  type Error = String;
+
+  // GET /search?name=abc&sort=1 HTTP/1.1
+  fn try_from(value: &[u8]) -> Result <Self, Self::Error> {
+    unimplemented!()
+  }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,7 @@
 use std::net::TcpListener;
+use std::convert::TryFrom;
 use std::io::Read;
+use crate::http::Request;
 pub struct Server {
   addr: String,
 }
@@ -21,7 +23,12 @@ impl Server {
             let mut buffer = [0; 1024]; // mutable buffer for .read to write 1024 is ok for our simple server
             match stream.read(&mut buffer) {
               Ok(_) => {
-                println!("Received a request: {}", String::from_utf8_lossy(&buffer))
+                println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                match Request::try_from(&buffer[..]) {//pass in a byte slice because trait is expecting
+                  Ok(request) => {},
+                  Err(e) => println!("Failed to parse a request: {}", e),
+                }  
               },
               Err(e) => println!("Failed to read from connection: {}", e),
             }
