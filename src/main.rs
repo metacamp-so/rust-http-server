700fb2fb27f6ebcbbae576da99145911b763b4f2
jakeyvee <jakeyvee@gmail.com>
2022-06-29 02:07:58 +0800

    read public path from environment variables

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use server::Server;
 use website_handler::WebsiteHandler;
+use std::env;
 
 mod server;
 mod http;
 mod website_handler;
 fn main() {
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+    println!("public_path: {}", public_path);
     let server = Server::new("127.0.0.1:8080".to_owned()); // Server is a struct, new is an associated function
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
